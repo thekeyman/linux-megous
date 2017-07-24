f2a0c10fc79e38b1128eef6d0170827cff984254
Maxime Ripard <maxime.ripard@free-electrons.com>
2017-07-24 18:49:43 +0200

    drm/sun4i: Add LVDS support
    The TCON supports the LVDS interface to output to a panel or a bridge.
    Let's add support for it.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

---
+++ drivers/gpu/drm/sun4i/sun4i_lvds.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2015 NextThing Co
+ * Copyright (C) 2015-2017 Free Electrons
+ *
+ * Maxime Ripard <maxime.ripard@free-electrons.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+#ifndef _SUN4I_LVDS_H_
+#define _SUN4I_LVDS_H_
+
+int sun4i_lvds_init(struct drm_device *drm, struct sun4i_tcon *tcon);
+
+#endif /* _SUN4I_LVDS_H_ */
