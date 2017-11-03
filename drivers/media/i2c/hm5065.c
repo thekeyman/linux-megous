f7314743ab6f04dee88d3debea5cb6410bdeb9b0
Ondrej Jirman <megous@megous.com>
2017-11-03 15:01:38 +0100

    media: hm5065: Drop derived from, code is very different now

--- drivers/media/i2c/hm5065.c
+++ drivers/media/i2c/hm5065.c
@@ -2,10 +2,6 @@
  * Himax HM5065 driver.
  * Copyright (C) 2017 Ondřej Jirman <megous@megous.com>.
  *
- * Derived from hm5065.c:
- * Copyright (C) 2011-2013 Freescale Semiconductor, Inc. All Rights Reserved.
- * Copyright (C) 2014-2017 Mentor Graphics Inc.
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
